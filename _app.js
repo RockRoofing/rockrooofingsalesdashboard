b2f14f5af0ee1ac3c0388dfa051014e307bea3e3
RockRoofing <james@rockroofing.co.uk>
2026-06-28 23:28:27 +0100

    Delete _app.js

--- _app.js
+++
@@ -1,7 +0,0 @@
-import { getCachedDeals, getLastSync } from '../../lib/db'
-
-export default async function handler(req, res) {
-  const deals = await getCachedDeals() || []
-  const lastSync = await getLastSync()
-  return res.status(200).json({ deals, lastSync })
-}
