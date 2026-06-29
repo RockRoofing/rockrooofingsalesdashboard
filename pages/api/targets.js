ea9f48ea1b0eec91ec1470fe93d306019e1641e3
RockRoofing <james@rockroofing.co.uk>
2026-06-29 06:36:24 +0100

    Create targets.js

---
+++ pages/api/targets.js
@@ -0,0 +1,38 @@
+import { get, set } from '../../lib/db'
+
+const DEFAULT_TARGETS = {
+  estimator: {
+    strikeRateOverall: 0.25,
+    strikeRateMCSecured: 0.30,
+    strikeRateMCUnsecured: 0.05,
+    valuePricedExisting: 300000,
+    totalValuePriced: 667000,
+    totalValueSecured: 133000,
+    dealsSecuredOver200k: 1,
+  },
+  sales: {
+    gleniganReceived: 6,
+    gleniganPriced: 3,
+    gleniganScored5: 3,
+    websiteReceived: 7,
+    websitePriced: 4,
+    strikeRateValue: 0.25,
+    valuePricedExisting: 800000,
+    totalValuePriced: 2000000,
+    projectsPricedOver200k: 9,
+    totalValueSecured: 400000,
+    projectsSecuredOver200k: 3,
+  }
+}
+
+export default async function handler(req, res) {
+  if (req.method === 'GET') {
+    const stored = await get('scorecard:targets')
+    return res.status(200).json({ targets: stored || DEFAULT_TARGETS })
+  }
+  if (req.method === 'POST') {
+    await set('scorecard:targets', req.body.targets)
+    return res.status(200).json({ success: true })
+  }
+  res.status(405).end()
+}
